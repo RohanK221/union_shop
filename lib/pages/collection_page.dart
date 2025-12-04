e13b41beb7752372c9d8ba816314d6a35db001f5
RohanK221 <rohan-kellaway@hotmail.com>
2025-12-04 21:03:05 +0000

    Create collections page

---
+++ lib/pages/collection_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/data/product_data.dart';
+import 'package:union_shop/main.dart';
+import 'package:union_shop/widgets/layout.dart';
+
+class CollectionPage extends StatelessWidget {
+  const CollectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final bool isSmallScreen = screenWidth < 600;
+
+    return MainLayout(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Our Collection',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 48),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: isSmallScreen ? 2 : 4,
+                crossAxisSpacing: 24,
+                mainAxisSpacing: 48,
+                childAspectRatio: 0.7,
+              ),
+              itemCount: allProducts.length,
+              itemBuilder: (context, index) {
+                return ProductCard(product: allProducts[index]);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
